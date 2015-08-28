af04b8b836afdf75c6a5bd45ac6a086b7aaedaa0
mmo218 <mmo218@lehigh.edu>
2015-08-28 23:07:27 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//////////////////////
+//Megan Olivola
+//hw01 assignment
+//Welcome Class program
+
+//define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main (String[] args) {
+
+//print the statement found in the hw assignment
+    System.out.println("\t -----------"); // tabbing this over so it's centered better
+    System.out.println("\t | WELCOME |"); 
+    System.out.println("\t -----------");
+    System.out.println("       ^  ^  ^  ^  ^  ^");
+    System.out.println("      / \\/ \\/ \\/ \\/ \\/ \\"); // \ is a special character and needs to be escaped with "\"
+    System.out.println("     <-m--m--o--2--1--8->");
+    System.out.println("      \\ /\\ /\\ /\\ /\\ /\\ /"); // \ is a special character and needs to be escaped with "\"
+    System.out.println("       v  v  v  v  v  v");
+    System.out.println("       I am very involved on campus.  I am on the dance team, I am a class officer, and I am a tutor for Economics.");
+    
+    //test/testtest
+    }
+}        
