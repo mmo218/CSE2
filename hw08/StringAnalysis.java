e92c5dbcd8f4cdfd9b3f7b8d12f7b3d952a25801
mmo218 <mmo218@lehigh.edu>
2015-10-21 02:03:16 +0000

    Updated hw08

--- hw08/StringAnalysis.java
+++ hw08/StringAnalysis.java
@@ -14,44 +14,67 @@ public class StringAnalysis{
         String input = ""; //declare string
         System.out.print("Enter a string: "); //prompt user for input
         input = MyScanner.nextLine();
-        System.out.print("If you want to look at a specific amount, please enter that amount.: ");
+        System.out.print("If you want to look at a specific amount, please enter that amount. (Otherwise, type no): ");
         if (MyScanner.hasNextInt()){
                int length = 0;
                length = MyScanner.nextInt();
                boolean chars = checkchar(input, length); //take the inputs of what the user entered and the previously entered string
                 if (chars == false){
-                    System.out.println("Not all characters entered are letters.");
+                    System.out.println("Not all characters entered are letters."); //not all chars are letter
                     }
                 else {
-                    System.out.println("All characters entered are letters.");
+                    System.out.println("All characters entered are letters."); //all chars are letters
                     }
                     
                 }
+        else if (MyScanner.hasNextInt() == false){
+            boolean chars = checkchar(input); //only input is the inputted string
+            if (chars == false){
+                    System.out.println("Not all characters entered are letters."); //not all chars are letter
+                    }
+                else {
+                    System.out.println("All characters entered are letters."); //all chars are letters
+                    }
+        }
                
-           } //end of main method
+    } //end of main method
 //create other methods
 //method check char with string
 
 public static boolean checkchar(String sentence, int total){
     
-    if (total>sentence.length()) {
-        for (int i =0; i < sentence.length(); i++) {
-        if (Character.isLetter(sentence.charAt(i))==false) {
-            return false;
+    if (total>sentence.length()){ //if the number entered is bigger than the total length
+        for (int i =0; i < sentence.length(); i++) { //just compare for the whole length 
+        if (Character.isLetter(sentence.charAt(i))==false){ //if it is not a letter
+            return false; //return false
             }
         }
-        return true;
+        return true; //otherwise return true
     }
     
-    else {
-        for(int i =0; i < total; i++) {
-        if(!Character.isLetter(sentence.charAt(i))==false) {
-            return false;
+    else { //if number entered is less than the total length
+        for(int i =0; i < total; i++){ //just look at the number of characters the user wants you to look at 
+        if(Character.isLetter(sentence.charAt(i))==false){ //if it not a letter 
+            return false;//return false
             }
+        
+        return true; //otherwise return true
         }
-        return true;
-    }
     
-}//end of checkchar method
+    }
+    return true;
+    
+    }//end of checkchar method
+    
+public static boolean checkchar(String sentence){
+    for (int i =0; i < sentence.length(); i++){ //just compare for the whole length 
+        if (Character.isLetter(sentence.charAt(i))==false){ //if it is not a letter
+            return false; //return false
+            }
+        return true; //otherwise return true
+    }
+    return true;
+} //end of checkchar method
+    
 
 }//end of class
